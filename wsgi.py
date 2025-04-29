18325aebf6161456677055154c88f40230e4e016
abishekdebug <142586462+abishekdebug@users.noreply.github.com>
2025-04-29 21:46:16 +0530

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,7 @@
 from app import app  # Import your Flask app instance
+import os  # Import os to access environment variables
 
 if __name__ == "__main__":
     app.config['TEMPLATES_AUTO_RELOAD'] = True
-    app.run()
+    # Make sure Flask runs on 0.0.0.0 and uses the port from environment variable
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
